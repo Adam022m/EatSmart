5e35c63dea250a474b6ef34b62d4b2764054916f
Adam_022m <lavitre256@gmail.com>
2024-11-23 23:15:12 +0100

    Ajout de la version v2.0

---
+++ EatSmart v2.0/variable.php
@@ -0,0 +1,55 @@
+<?php
+$sandwich = [
+    [
+        'nom' => 'Boursin',
+        'description' => 'Description : Escalope, sauce boursin, avec frites et boissons',
+        'Prix' =>  ' Prix : 7,90€',
+        'image' => '"./assets/sandwich boursin.jfif"'
+    ],
+    [
+        'nom' => 'Suisse',
+        'description' => 'Description : Escalope ou tenders au choix, crème fraiche, avec frites et boissons',
+        'Prix' => ' Prix : 6,50€',
+        'image' => '"./assets/suisse sandwich.webp"'
+    ],
+    [
+        'nom' => 'Curry',
+        'description' => 'Description : Escalope, sauce curry, avec frites et boissons',
+        'Prix' => ' Prix : 6,90€',
+        'image' => '"./assets/curry sandwich.jfif"'
+    ]
+];
+$texmex = [
+    [
+        'nom' => 'Samboussa',
+        'description' => 'Description : Viande hachée pimentée enroulé d\'une fine pate croustillante',
+        'Prix' => ' Prix : 0,50€ l`\unité',
+        'image' => '"./assets/samboussa.jfif"'
+    ],
+    [
+        'nom' => 'Nems',
+        'description' => 'Description : Nems à l\'unité',
+        'Prix' => ' Prix : 0,50€ l\'unité',
+        'image' => '"./assets/nems.jfif"'
+    ],
+    [
+        'nom' => 'Nuggets',
+        'description' => 'Description : Nuggets frits à l\'unité',
+        'Prix' => ' Prix : 0,80€ l\'unité',
+        'image' => '"./assets/nuggets.jfif"'
+    ]
+];
+$dessert = [
+    [
+        'nom' => 'Pot de glace au chocolat',
+        'description' => 'Description : Glace au chocolat dans un pot de 500ml, cuillère fournie',
+        'Prix' => ' Prix : 1,50€',
+        'image' => '"./assets/pot glace chocolat.jfif"'
+    ],
+    [
+        'nom' => 'Pot de glace à la vanille',
+        'description' => 'Description : Glace à la vanile dans un pot de 500ml, cuillère fournie',
+        'Prix' => 'Prix : 1,50€',
+        'image' => '"./assets/pot glace vanille.jfif"'
+    ]
+];
